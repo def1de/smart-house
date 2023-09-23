e7fa0505d92b6369493da57b89f4e6b62cb04fb6
def1de <iliakat.kr@gmail.com>
2023-09-23 17:27:07 +0100

    video support

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
+from ..camera import gen_frames
 
 web = Blueprint("web", __name__, template_folder="templates", static_folder="static")
 
 @web.route("/")
 def index():
-    return render_template("index.html", data = [
-        {'column1': 'Value1', 'column2': 'Value2', 'column3': 'Value3'},
-        {'column1': 'Value4', 'column2': 'Value5', 'column3': 'Value6'}])
+    return render_template("index.html")
+
+@web.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
