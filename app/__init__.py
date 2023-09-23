2c290ca653db8d744542ca9b4b7e9b1b5326e6f8
def1de <iliakat.kr@gmail.com>
2023-09-23 14:01:54 +0100

    added html templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from .extentions import api, db
 from .resources import ns
+from .web.routes import web
 
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(web, url_prefix="/")
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
     api.init_app(app)
     db.init_app(app)
-    db.create_all()
 
     api.add_namespace(ns)
 
