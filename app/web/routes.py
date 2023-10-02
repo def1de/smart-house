56292da3c25a0f72dabe970a12c0e426cf218e9d
def1de <iliakat.kr@gmail.com>
2023-10-02 13:15:10 +0100

    structure changes

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, Response
-from ..camera import gen_frames
+from .camera import gen_frames
 
 web = Blueprint("web", __name__, template_folder="templates", static_folder="static")
 
